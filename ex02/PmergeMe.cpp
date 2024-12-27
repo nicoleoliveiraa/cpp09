1cb6e922a6463d7621910e38774f16928fb8125f
Nicole Oliveira <nicole.s.oliveira26@gmail.com>
2024-12-27 12:07:14 +0000

    feat: implement PmergeMe class with constructors and algorithm methods

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsouza-o <nsouza-o@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 11:30:56 by nsouza-o          #+#    #+#             */
+/*   Updated: 2024/12/26 11:34:58 by nsouza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(){}
+
+PmergeMe::PmergeMe(const PmergeMe& src)
+{
+	*this = src;	
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& src)
+{
+	if (this != &src)
+		*this = src;
+	return (*this);
+}
+
+PmergeMe::~PmergeMe(){}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: nsouza-o <nsouza-o@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 14:56:17 by nsouza-o          #+#    #+#             */
-/*   Updated: 2024/12/20 14:59:43 by nsouza-o         ###   ########.fr       */
+/*   Updated: 2024/12/27 11:59:12 by nsouza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,37 @@
 
 class PmergeMe {
 private:
+	template <typename T> void _fordJohnsonAlgorithm(T& container, size_t nbrElem);
+	template <typename T> void _swapPairs(T& container, size_t nbrElem);
 
 public:
 	PmergeMe();
 	PmergeMe(const PmergeMe& src);
 	PmergeMe& operator=(const PmergeMe& src);
 	~PmergeMe();
+
 };
 
+template <typename T> void PmergeMe::_swapPairs(T& container, size_t nbrElem)
+{
+	typedef typename T::iterator Iterator;
+	
+	int nbrOfElem = container.size() / nbrElem;
+	
+}
+
+
+template <typename T> void PmergeMe::_fordJohnsonAlgorithm(T& container, size_t nbrElem)
+{
+	typedef typename T::iterator Iterator;
+	
+	if (container.size() / nbrElem < 2)
+		return ;
+	
+	_swapPairs(container, nbrElem);
+	_fordJohnsonAlgorithm(container, nbrElem);	
+	
+}
 
 
 #endif // PMERGEME_HPP
